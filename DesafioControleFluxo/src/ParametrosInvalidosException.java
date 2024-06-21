4470d008bdc475f692e346dde334e6d629560153
anthonyfelipe24 <anthonyfelipe149@gmail.com>
2024-06-21 18:03:35 -0300

    PROJETO NOVO

---
+++ DesafioControleFluxo/src/ParametrosInvalidosException.java
@@ -0,0 +1,8 @@
+public class ParametrosInvalidosException extends Exception {
+    public ParametrosInvalidosException(String message, Object newParam, Object newParam2) {
+        super(message);
+    }
+
+    
+        }
+
